9fd030e5d111d00db01d02e5be48add1043ff53c
zyrab <zurakrua@gmail.com>
2026-01-18 01:07:51 +0100

    ref: updated input compnent visuals

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -26,7 +26,7 @@ function Input({ className, type, startIcon, endIcon, ...props }: InputProps) {
           "dark:bg-input/30 border-input focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 disabled:bg-input/50 dark:disabled:bg-input/80 h-9 rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:h-6 file:text-xs file:font-medium focus-visible:ring-1 aria-invalid:ring-1 md:text-sm file:text-foreground placeholder:text-muted-foreground w-full min-w-0 outline-none file:inline-flex file:border-0 file:bg-transparent disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
           startIcon ? "pl-9" : "",
           endIcon ? "pr-9" : "",
-          className
+          className,
         )}
         {...props}
       />
@@ -46,8 +46,8 @@ function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimiti
     <LabelPrimitive.Root
       data-slot="label"
       className={cn(
-        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
-        className
+        "text-sm mb-3 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        className,
       )}
       {...props}
     />
@@ -68,7 +68,7 @@ function InputGroup({ label, error, id, containerClassName, className, ...props 
 
   return (
     // Added 'w-full' here to ensure it tries to fill the parent width
-    <div className={cn("space-y-2 w-full", containerClassName)}>
+    <div className={cn("w-full", containerClassName)}>
       {label && <Label htmlFor={inputId}>{label}</Label>}
 
       <Input
@@ -93,7 +93,7 @@ function Checkbox({ className, ...props }: React.ComponentProps<typeof CheckboxP
       className={cn(
         // Changed rounded-none to rounded-sm for better aesthetics, revert if you prefer square
         "border-input dark:bg-input/30 data-checked:bg-primary data-checked:text-primary-foreground dark:data-checked:bg-primary data-checked:border-primary aria-invalid:aria-checked:border-primary aria-invalid:border-destructive dark:aria-invalid:border-destructive/50 focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 flex size-4 items-center justify-center rounded-sm border transition-colors group-has-disabled/field:opacity-50 focus-visible:ring-1 aria-invalid:ring-1 peer relative shrink-0 outline-none after:absolute after:-inset-x-3 after:-inset-y-2 disabled:cursor-not-allowed disabled:opacity-50",
-        className
+        className,
       )}
       {...props}
     >
