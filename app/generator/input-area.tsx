aac3f9d463453626978a6ad19805e09cfda3be1a
zyrab <zurakrua@gmail.com>
2026-02-22 01:11:10 +0100

    feat: added acordeons and qr type selection in input fields

--- app/generator/input-area.tsx
+++ app/generator/input-area.tsx
@@ -1,33 +1,65 @@
 import * as React from "react";
-import { InputGroup } from "@/components/ui/input";
+import { CheckboxLabel, InputGroup } from "@/components/ui/input";
 import { QRContent } from "@/types/qr";
 import Icons from "@/components/elements/icons";
+import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import RadioTexts from "@/components/elements/radio-text";
 
 interface InputAreaProps {
   content: QRContent;
   name: string;
-  onContentChange: (field: string, value: string) => void;
+  onContentChange: (field: string, value: string | boolean) => void;
   onNameChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleContentTypeChange: (type: QRContent["type"]) => void;
   t: any;
 }
 
-export default function InputArea({ content, name, onContentChange, onNameChange, t }: InputAreaProps) {
+export default function InputArea({
+  content,
+  name,
+  onContentChange,
+  onNameChange,
+  handleContentTypeChange,
+  t,
+}: InputAreaProps) {
   const activeFields = INPUT_FIELDS[content.type as keyof typeof INPUT_FIELDS] || [];
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4">
-      <InputGroup label={t.name.text} value={name} onChange={(e) => onNameChange(e)} placeholder={t.name.placeholder} />
-      {activeFields.map(({ key, icon, requred, type }, i) => (
-        <InputGroup
-          key={i}
-          label={t[key].text}
-          value={(content as any)[key] || ""}
-          onChange={(e) => onContentChange(key, e.target.value)}
-          placeholder={t[key].placeholder}
-          startIcon={<Icons name={icon} size={16} />}
-          required={requred}
-        />
-      ))}
-    </div>
+    <AccordionItem value="item-1">
+      <AccordionTrigger>Content</AccordionTrigger>
+      <AccordionContent>
+        <RadioTexts values={["url", "text", "wifi"]} value={content.type} onValueChange={handleContentTypeChange} />
+
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-4 mt-2">
+          <InputGroup
+            label={t.name.text}
+            value={name}
+            onChange={(e) => onNameChange(e)}
+            placeholder={t.name.placeholder}
+          />
+          {activeFields.map(({ key, icon, requred, type }, i) =>
+            type !== "checkbox" ? (
+              <InputGroup
+                key={i}
+                label={t[key].text}
+                type={type}
+                value={(content as any)[key] || ""}
+                onChange={(e) => onContentChange(key, e.target.value)}
+                placeholder={t[key].placeholder}
+                startIcon={<Icons name={icon} size={16} />}
+                required={requred}
+              />
+            ) : (
+              <CheckboxLabel
+                key={i}
+                label={t[key].text}
+                id={key}
+                onCheckedChange={(c: boolean) => onContentChange(key, c)}
+              />
+            ),
+          )}
+        </div>
+      </AccordionContent>
+    </AccordionItem>
   );
 }
 
@@ -37,6 +69,7 @@ export const INPUT_FIELDS = {
   wifi: [
     { key: "ssid", icon: "wifi", type: "text", requred: true },
     { key: "password", icon: "key_round", type: "text", requred: false },
+    { key: "hidden", icon: "", type: "checkbox", requred: false },
   ],
   // email: [ { key: "email", labelKey: "email.addr", ... }, { key: "subject", ... } ]
 };
