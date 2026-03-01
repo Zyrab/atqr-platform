e453b86b374d60abc5bc431e78ebd93aa26a3961
zyrab <zurakrua@gmail.com>
2026-03-01 23:59:00 +0100

    ref: updated badgecomponent

--- app/dashboard/qr-item.tsx
+++ app/dashboard/qr-item.tsx
@@ -1,23 +1,6 @@
 "use client";
 
-import { useState, useRef, useMemo } from "react";
-import {
-  Download,
-  Edit,
-  Trash2,
-  Copy,
-  Calendar,
-  Loader2,
-  Link2,
-  Check,
-  X,
-  CornerDownRight,
-  Palette,
-  ChartLine,
-  Type,
-  Wifi,
-  Text,
-} from "lucide-react";
+import { useState, useRef } from "react";
 
 // UI Components
 import { Card } from "@/components/ui/card";
@@ -35,6 +18,8 @@ import { useQR } from "@/context/qr-context";
 import { useQRCodeGenerator } from "@/hooks/use-qr-generator";
 import { QRContent, QRDocument } from "@/types/qr";
 import { contentCheckers, resolveQRString } from "@/lib/content-utils";
+import Icons from "@/components/elements/icons";
+import { ContentBadge } from "./content-badge";
 
 interface DashboardItemProps {
   item: QRDocument;
@@ -43,11 +28,6 @@ interface DashboardItemProps {
   onDuplicate: (item: QRDocument) => void;
   t: any;
 }
-const dateFormatter = new Intl.DateTimeFormat("en-US", {
-  month: "short",
-  day: "numeric",
-  year: "numeric",
-});
 
 export default function DashboardItem({ item, onEdit, onDelete, onDuplicate, t }: DashboardItemProps) {
   const [downloadSize, setDownloadSize] = useState(1000);
@@ -74,70 +54,16 @@ export default function DashboardItem({ item, onEdit, onDelete, onDuplicate, t }
     downloadQrCode(svgRef, fileName, downloadFormat, downloadSize);
   };
 
-  const formattedDate = useMemo(() => {
-    return dateFormatter.format(new Date(item.createdAt));
-  }, [item.createdAt]);
-
-  function renderContentBadges(item: QRDocument) {
-    const content = item.content;
-    if (item.type === "dynamic" && content.type === "url")
-      return (
-        <>
-          <Badge variant="ghost">
-            <Link2 />
-            {qrValue?.slice(0, 25) + "…"}
-          </Badge>
-          <Badge variant="ghost">
-            <CornerDownRight />
-            {content.url.slice(0, 25) + "…"}
-          </Badge>
-        </>
-      );
-    switch (content.type) {
-      case "url":
-        return (
-          <Badge variant="ghost">
-            <CornerDownRight />
-            {content.url.slice(0, 25) + "…"}
-          </Badge>
-        );
-      case "text":
-        return (
-          <Badge variant="ghost">
-            <Text />
-            {content.text.slice(0, 25) + "…"}
-          </Badge>
-        );
-      case "wifi":
-        return (
-          <Badge variant="ghost">
-            <Wifi />
-            SSID: {content.ssid || "—"}, Password: {content.password || "—"}
-          </Badge>
-        );
-      default:
-        return null;
-    }
-  }
-
-  const typeIcon = {
-    url: <Link2 />,
-    text: <Type />,
-    wifi: <Wifi />,
-    dynamic: <Link2 />,
-  };
-
   return (
     <Card width="auto" size="sm" className="gap-8 md:flex-row md:justify-between">
-      {/* 1. Visual Preview Area */}
       <div className="gap-8 flex flex-row">
         <div className="flex flex-col gap-2 md:flex-row md:gap-5">
-          <div className="w-30 h-30 transition-transform duration-300 group-hover:scale-105">
+          <div className="w-35 h-35 transition-transform duration-300 group-hover:scale-105">
             <QRCodeRenderer matrix={matrix} svgRef={svgRef} size={200} design={item.design} />
           </div>
           <div className="flex flex-row md:flex-col  md:pt-2 justify-between md:justify-start gap-3">
             <Badge variant="secondary" className="md:w-full">
-              {typeIcon[item.content.type]}
+              <Icons name={item.content.type} />
               {item.content.type.toUpperCase()}
             </Badge>
 
@@ -155,7 +81,7 @@ export default function DashboardItem({ item, onEdit, onDelete, onDuplicate, t }
           {!isEditName ? (
             <HeaderGroup tag="h3" header={item.name || "Untitled QR"} className="flex-row items-start">
               <Button variant="ghost" onClick={() => setIsEditName(true)}>
-                <Edit />
+                <Icons name="edit" />
               </Button>
             </HeaderGroup>
           ) : (
@@ -170,44 +96,40 @@ export default function DashboardItem({ item, onEdit, onDelete, onDuplicate, t }
                   setIsEditName(false);
                 }}
               >
-                <Check />
+                <Icons name="check" />
               </Button>
 
               <Button
                 size="sm"
                 variant="ghost"
                 onClick={() => {
-                  setNameDraft(item.name); // reset
+                  setNameDraft(item.name);
                   setIsEditName(false);
                 }}
               >
-                <X />
+                <Icons name="x" />
               </Button>
             </div>
           )}
-          {renderContentBadges(item)}
-          <Badge variant="ghost">
-            <Calendar />
-            {formattedDate}
-          </Badge>
+          <ContentBadge doc={item} qrValue={qrValue} />
         </div>
       </div>
       <div className="gap-8 flex flex-col md:flex-row">
         <div className="flex flex-col justify-between">
           <div className="flex flex-row gap-0.5 justify-between">
             <Button variant="ghost" size="lg" onClick={() => onEdit(item.id)}>
-              <Palette />
+              <Icons name="palette" />
             </Button>
             {isDynamic && (
               <Button variant="ghost" size="lg">
-                <ChartLine />
+                <Icons name="chartLine" />
               </Button>
             )}
             <Button variant="ghost" size="lg" onClick={() => onDuplicate(item)}>
-              <Copy />
+              <Icons name="copy" />
             </Button>
             <Button variant="ghost" size="lg" onClick={() => onDelete(item.id)}>
-              <Trash2 color="#C9404A" />
+              <Icons name="trash_2" color="#C9404A" />
             </Button>
           </div>
           <RadioTexts values={["png", "jpeg", "svg"]} value={downloadFormat} onValueChange={setDownloadFormat} />
@@ -224,7 +146,7 @@ export default function DashboardItem({ item, onEdit, onDelete, onDuplicate, t }
             className="w-full"
           />
           <Button onClick={handleDownload} disabled={isDownloading} size="lg">
-            {isDownloading ? <Loader2 className="animate-spin h-4 w-4 mr-2" /> : <Download className="w-4 h-4 mr-2" />}
+            {isDownloading ? <Icons name="loader_2" className="animate-spin" /> : <Icons name="download" />}
             {t.action.label}
           </Button>
         </div>
