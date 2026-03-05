2e63574abb8e5835de49062d06d424eadf0f16dc
zyrab <zurakrua@gmail.com>
2026-03-05 12:53:38 +0100

    feat: added notify provider for centrilised user feedbacks

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/auth-context";
 import { QRProvider } from "@/context/qr-context";
+import { NotifyProvider } from "@/context/notify-context";
 import Header from "@/components/layout/headet";
 const fontSans = DM_Sans({
   weight: ["400", "500", "700"],
@@ -20,18 +21,20 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en">
       <body className={`${fontSans.variable} font-sans antialiased bg-background text-foreground`}>
-        <AuthProvider>
-          <QRProvider>
-            <Header />
-            <main className="flex flex-col min-h-[80vh] mt-12 gap-24">{children}</main>
+        <NotifyProvider>
+          <AuthProvider>
+            <QRProvider>
+              <Header />
+              <main className="flex flex-col min-h-[80vh] mt-12 gap-24">{children}</main>
 
-            <footer className="bg-muted/30 border-t border-border py-8 mt-12">
-              <div className="container mx-auto px-4 text-center">
-                <p className="text-sm text-muted-foreground">© 2024 At QR. Built for simplicity.</p>
-              </div>
-            </footer>
-          </QRProvider>
-        </AuthProvider>
+              <footer className="bg-muted/30 border-t border-border py-8 mt-12">
+                <div className="container mx-auto px-4 text-center">
+                  <p className="text-sm text-muted-foreground">© 2024 At QR. Built for simplicity.</p>
+                </div>
+              </footer>
+            </QRProvider>
+          </AuthProvider>
+        </NotifyProvider>
       </body>
     </html>
   );
